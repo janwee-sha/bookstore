f65e969dbd08cdde9b6ea64047b0ace124fef3d3
xiazhangwei <janwee_sha@163.com>
2022-05-04 20:20:29 +0800

    add Dockerfile;delete redundant dependencies management items.

--- book-server/src/main/java/com/janwee/bookstore/bookserver/application/BookApplicationService.java
+++ book-server/src/main/java/com/janwee/bookstore/bookserver/application/BookApplicationService.java
@@ -4,6 +4,8 @@ package com.janwee.bookstore.bookserver.application;
 import com.janwee.bookstore.bookserver.domain.AuthorService;
 import com.janwee.bookstore.bookserver.domain.Book;
 import com.janwee.bookstore.bookserver.domain.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +14,7 @@ import java.util.Optional;
 
 @Service
 public class BookApplicationService {
+    private static final Logger log = LoggerFactory.getLogger(BookApplicationService.class);
     private final BookRepository bookRepo;
     private final AuthorService authorService;
 
@@ -23,16 +26,25 @@ public class BookApplicationService {
 
     @Transactional(readOnly = true, rollbackFor = Throwable.class)
     public Optional<BookInfo> book(Long id) {
+        log.info("Loading book with ID: {}.", id);
         return bookRepo.findById(id).map(book -> new BookInfo(book, authorService.author(book.getAuthorId())));
     }
 
     @Transactional(rollbackFor = Throwable.class)
     public void add(Book book) {
+        log.info("Adding book.");
+        bookRepo.save(book);
+    }
+
+    @Transactional(rollbackFor = Throwable.class)
+    public void modify(Book book) {
+        log.info("Modifying book.");
         bookRepo.save(book);
     }
 
     @Transactional(rollbackFor = Throwable.class)
     public void remove(Long id) {
+        log.info("Removing book with ID: {}.", id);
         bookRepo.deleteById(id);
     }
 }
