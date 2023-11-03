3329695e0d3fea93f3fec82831f905659ecc8354
Jan-wee Sha <64795190+janwee-sha@users.noreply.github.com>
2023-11-03 19:08:23 +0800

    rename modules.

---
+++ author/src/test/java/com/janwee/bookstore/authorserver/AuthorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janwee.bookstore.authorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ book/src/main/java/com/janwee/bookstore/bookserver/infrastructure/service/TranslatingAuthorService.java
@@ -0,0 +1,45 @@
+package com.janwee.bookstore.bookserver.infrastructure.service;
+
+import com.janwee.bookstore.bookserver.domain.Author;
+import com.janwee.bookstore.bookserver.domain.AuthorService;
+import com.janwee.bookstore.bookserver.infrastructure.feign.AuthorFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class TranslatingAuthorService implements AuthorService {
+    private final AuthorFeignClient authorFeignClient;
+
+    @Autowired
+    public TranslatingAuthorService(AuthorFeignClient authorFeignClient) {
+        this.authorFeignClient = authorFeignClient;
+    }
+
+    @Override
+    @CircuitBreaker(name = "authorServer", fallbackMethod = "fallbackAuthor")
+    public Author author(Long authorId) {
+        randomlyRunLong();
+        return authorFeignClient.author(authorId);
+    }
+
+    private Author fallbackAuthor(Long authorId) {
+        return new Author().withId(authorId).withName("Sorry no author currently available");
+    }
+
+    private void randomlyRunLong() {
+        if (new Random().nextInt(3) + 1 == 3) {
+            sleep();
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
