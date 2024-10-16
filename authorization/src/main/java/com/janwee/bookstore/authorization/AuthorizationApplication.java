ba90fb54863e29b6ac6f4a6bee0a083275411f2f
janwee <64795190+janwee-sha@users.noreply.github.com>
2024-10-17 05:07:47 +0800

    Authorization module.

--- authorization/src/main/java/com/janwee/bookstore/authorization/AuthorizationApplication.java
+++ authorization/src/main/java/com/janwee/bookstore/authorization/AuthorizationApplication.java
@@ -2,12 +2,8 @@ package com.janwee.bookstore.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-@RefreshScope
 public class AuthorizationApplication {
 
     public static void main(String[] args) {
