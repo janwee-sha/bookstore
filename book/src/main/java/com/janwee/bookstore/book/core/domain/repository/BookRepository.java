ffe660470419ca90c92d3300d3ce02b46b5ebae3
Janwee <64795190+janwee-sha@users.noreply.github.com>
2026-05-13 00:34:29 +0800

    Book repository.

--- book/src/main/java/com/janwee/bookstore/book/core/domain/repository/BookRepository.java
+++ book/src/main/java/com/janwee/bookstore/book/core/domain/repository/BookRepository.java
@@ -2,7 +2,6 @@ package com.janwee.bookstore.book.core.domain.repository;
 
 import com.janwee.bookstore.book.core.domain.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
