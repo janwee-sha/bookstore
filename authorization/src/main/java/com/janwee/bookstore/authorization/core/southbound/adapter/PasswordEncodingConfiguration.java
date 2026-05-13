4b35c33df6018f1404ee7c74d3847dbee989061e
Janwee <64795190+janwee-sha@users.noreply.github.com>
2026-05-14 00:49:11 +0800

    Move password encoder configuration to southbound adapter layer.

--- authorization/src/main/java/com/janwee/bookstore/authorization/core/southbound/adapter/PasswordEncodingConfiguration.java
+++ authorization/src/main/java/com/janwee/bookstore/authorization/core/southbound/adapter/PasswordEncodingConfiguration.java
@@ -1,4 +1,4 @@
-package com.janwee.bookstore.authorization.security;
+package com.janwee.bookstore.authorization.core.southbound.adapter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
