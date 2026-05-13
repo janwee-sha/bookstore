6b73b2a79263b14d660039f080dc314f2444409e
Janwee <64795190+janwee-sha@users.noreply.github.com>
2026-05-14 00:44:36 +0800

    Split authorization security configuration

---
+++ authorization/src/main/java/com/janwee/bookstore/authorization/security/ApplicationWebSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.janwee.bookstore.authorization.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class ApplicationWebSecurityConfiguration {
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain applicationSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) ->
+                        authorize
+                                .requestMatchers("/public/**")
+                                .permitAll()
+                                .requestMatchers(
+                                        "/swagger-ui.html",
+                                        "/swagger-ui/**",
+                                        "/api-docs/**")
+                                .permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.maximumSessions(1)
+                        .expiredSessionStrategy(new UnauthorizedSessionExpiredStrategy(objectMapper)));
+
+        return http.build();
+    }
+}
