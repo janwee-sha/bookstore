58285ac4b6ca810c3041384bfd33b5a670030ea2
Janwee <64795190+janwee-sha@users.noreply.github.com>
2026-05-15 19:16:42 +0800

    Treat gateway as a OAuth 2 resource server.

--- gateway/src/main/java/com/janwee/bookstore/gateway/security/OAuth2LoginConfiguration.java
+++ gateway/src/main/java/com/janwee/bookstore/gateway/security/OAuth2LoginConfiguration.java
@@ -2,13 +2,15 @@ package com.janwee.bookstore.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class OAuth2LoginConfiguration {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Order(2)
+    public SecurityFilterChain oauth2LoginSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/", "/login/**").permitAll()
                         .anyRequest().authenticated()
