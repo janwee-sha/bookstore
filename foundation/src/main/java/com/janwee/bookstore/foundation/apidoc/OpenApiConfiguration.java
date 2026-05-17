ac328b52194d7d06ffb033d1da5139aa6bf957ab
Janwee <64795190+janwee-sha@users.noreply.github.com>
2026-05-18 00:31:10 +0800

    OpenAPI configuration.

--- foundation/src/main/java/com/janwee/bookstore/foundation/apidoc/OpenApiConfiguration.java
+++ foundation/src/main/java/com/janwee/bookstore/foundation/apidoc/OpenApiConfiguration.java
@@ -1,21 +1,25 @@
-package com.janwee.bookstore.book.apidoc;
+package com.janwee.bookstore.foundation.apidoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.method.HandlerMethod;
 
-@Configuration
+@AutoConfiguration
+@ConditionalOnClass({OpenAPI.class, OperationCustomizer.class})
 public class OpenApiConfiguration {
     private static final String BEARER_AUTH_SCHEME = "bearerAuth";
 
     @Bean
-    public OpenAPI bookOpenApi() {
+    @ConditionalOnMissingBean(OpenAPI.class)
+    public OpenAPI openApi() {
         SecurityScheme bearerAuthScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
@@ -27,6 +31,7 @@ public class OpenApiConfiguration {
     }
 
     @Bean
+    @ConditionalOnMissingBean(name = "bearerAuthOperationCustomizer")
     public OperationCustomizer bearerAuthOperationCustomizer() {
         return (operation, handlerMethod) -> {
             if (requiresAuthorization(handlerMethod)) {
