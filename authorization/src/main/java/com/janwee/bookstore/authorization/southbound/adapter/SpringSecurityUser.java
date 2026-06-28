e71964407af9979cecb1fc0e24ab547cd6839a62
Ian Hsia <zhangwei.xia@IandeMacBook-Pro.local>
2026-06-28 14:52:03 +0800

    删除冗余方法。

--- authorization/src/main/java/com/janwee/bookstore/authorization/southbound/adapter/SpringSecurityUser.java
+++ authorization/src/main/java/com/janwee/bookstore/authorization/southbound/adapter/SpringSecurityUser.java
@@ -55,24 +55,4 @@ public class SpringSecurityUser implements UserDetails {
     public String getUsername() {
         return email;
     }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
