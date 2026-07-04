289d149af0acd92d90f1c9da3eb06003f54138ea
Ian Hsia <zhangwei.xia@IandeMacBook-Pro.local>
2026-07-04 11:02:01 +0800

    发布政策服务类单元测试。

--- book/src/test/java/com/janwee/bookstore/book/domain/service/BookPublicationPolicyUnitTest.java
+++ book/src/test/java/com/janwee/bookstore/book/domain/service/BookPublicationPolicyUnitTest.java
@@ -1,51 +1,32 @@
 package com.janwee.bookstore.book.domain.service;
 
-import com.janwee.bookstore.book.domain.exception.BookNotFoundException;
 import com.janwee.bookstore.book.domain.exception.InvalidBookException;
 import com.janwee.bookstore.book.domain.model.Book;
 import com.janwee.bookstore.book.domain.repository.AuthorRepository;
-import com.janwee.bookstore.book.domain.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BookPublicationPolicyUnitTest {
 
     @Mock
-    private BookRepository bookRepo;
-
-    @Mock
     private AuthorRepository authorRepo;
 
     @InjectMocks
-    private BookPublicationPolicy validator;
+    private BookPublicationPolicy bookPublicationPolicy;
 
     @Test
     void shouldPassWhenExistingBookAndAuthorAreAvailable() {
         Book book = newBook(1L, 2L);
-        when(bookRepo.hasBookOf(1L)).thenReturn(true);
         when(authorRepo.hasAuthorOf(2L)).thenReturn(true);
 
-        assertDoesNotThrow(() -> validator.check(book));
-    }
-
-    @Test
-    void shouldRejectMissingBookWhenValidatingExistingBook() {
-        Book book = newBook(1L, 2L);
-        when(bookRepo.hasBookOf(1L)).thenReturn(false);
-
-        BookNotFoundException ex = assertThrows(BookNotFoundException.class,
-                () -> validator.check(book));
-
-        assertEquals("No such book of ID: 1", ex.getMessage());
+        assertDoesNotThrow(() -> bookPublicationPolicy.check(book));
     }
 
     @Test
@@ -54,7 +35,7 @@ class BookPublicationPolicyUnitTest {
         when(authorRepo.hasAuthorOf(2L)).thenReturn(false);
 
         InvalidBookException ex = assertThrows(InvalidBookException.class,
-                () -> validator.check(book));
+                () -> bookPublicationPolicy.check(book));
 
         assertEquals("No such author", ex.getMessage());
     }
